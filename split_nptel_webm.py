f2c037258da998b84bbdf3d48dfc5fd880c09b6f
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2021-10-15 17:08:03 +0530

    Finish data splitting

--- split_nptel_webm.py
+++ split_nptel_webm.py
@@ -1,7 +1,6 @@
 import os
-import sys
 import subprocess
-
+from datetime import datetime
 
 def get_subs_webm(dir):
     for root, dirs, files in os.walk(dir):
@@ -18,10 +17,14 @@ def get_subs_webm(dir):
 
                     while(linelist[start].find('-->') == -1):
                         start += 1
+                    ## skip the first occurrence, usually faulty
+                    start+=1
+                    while(linelist[start].find('-->') == -1):
+                        start += 1
 
                     i = start
                     while(i < len(linelist)):
-                        print(linelist[i])
+                        #print(linelist[i])
                         startstamp, endstamp = linelist[i].split('-->')
                         caption = ""
 
@@ -30,8 +33,8 @@ def get_subs_webm(dir):
                             caption += linelist[i]
                             i += 1
 
-                        stampslist.append({'timestamp': [startstamp, endstamp],
-                                           'captions': caption})
+                        stampslist['data'].append({'timestamp': [startstamp.strip(), endstamp.strip()],
+                                           'captions': " ".join(caption.split())})
                 yield stampslist
 
 
@@ -45,12 +48,33 @@ def convert_to_wav(dir):
                     ['ffmpeg', '-i', f'\"./nptel/DSA/{sub}\"', '-c:a', 'pcm_f32le', f'\".nptel/DSA/{name}\".wav'])
 
 
-def get_audio_from_subs(stampslist):
-    audio = stampslist['file'].split('.')[0]+".webm"
-    # TODO
+def get_audio_from_subs(stampslist,datadir='./nptel/DSA/'):
+    a_prefix = stampslist['file'].split('.')[0]
+    audio = a_prefix+".webm"
+    outdir = os.path.join(datadir,a_prefix)
+    os.makedirs(outdir, exist_ok=True)
+    
+    for data in stampslist['data']:
+
+        ss = datetime.strptime(data['timestamp'][0],"%H:%M:%S.%f")
+        t = datetime.strptime(data['timestamp'][1],"%H:%M:%S.%f") - ss
+        
+        audiofile = os.path.join(datadir,audio)
+        outfile = os.path.join(outdir,a_prefix+data['timestamp'][0]+'.webm')
+        
+        #append a singular zero to timedelta 
+        timed = "0"+str(t)[:-3]
+
+        # split the audio
+        subprocess.Popen(['ffmpeg','-i',f'{audiofile}','-ss', f'{datetime.strftime(ss,"%H:%M:%S.%f")}', '-t', f'{timed}',f'{outfile}'])
+        
+        # store the transcript
+        with open(os.path.join(outdir, a_prefix+data['timestamp'][0]+".txt"),'w') as f:
+            f.write(data['captions'])
+
 
 
 if __name__ == '__main__':
-    convert_to_wav('./nptel/DSA')
-    # lis = get_subs_webm('./nptel/DSA')
-    # print(next(lis))
+    # convert_to_wav('./nptel/DSA')
+    lis = get_subs_webm('./nptel/DSA')
+    get_audio_from_subs(next(lis))
